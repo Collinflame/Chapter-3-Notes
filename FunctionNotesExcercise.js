5a28b00a8eb7ed1c819f2c145ad77cdb789ac3a3
3siy <3siy@fresnou.org>
2023-03-14 14:19:09 -0700

    Function Update

---
+++ FunctionNotesExcercise.js
@@ -0,0 +1,41 @@
+//Question 1
+// function findAreaOfRectangle(l, w){
+//     console.log(`The area of the rectangle is ${l * w}`);
+// }
+// findAreaOfRectangle(3,4);
+
+//Question 2
+// function findAreaOfCircle(r){
+//     console.log(`The area of the circle is ${r * r * 3.14}`);
+// }
+// findAreaOfCircle(3);
+
+//Question 3
+// function findMin(num1, num2){
+//     if (num1 > num2) {
+//         console.log (`${num2} is the smaller number`)
+//     } else {
+//         console.log (`${num1} is the smaller number`)
+//     }
+// }
+// findMin(3, 99);
+
+//Question 4
+// function checkEquality(numb1, numb2){
+//     if (numb1 == numb2) {
+//         console.log (`The numbers are equal`)
+//     } else {
+//         console.log (`The numbers aren't equal`)
+//     }
+// }
+// checkEquality(4, 4.1);
+
+//Question 5
+// function calculateLargestRectangle(rec1l, rec2l, rec1w, rec2w){
+//     if (rec1l * rec1w > rec2l * rec2w) {
+//         console.log (`rectangle 1 is bigger and is ${rec1l * rec1w}`)
+//     } else {
+//         console.log (`rectangle 2 is bigger and is ${rec1l * rec1w}`)
+//     }
+// }
+// calculateLargestRectangle(4, 4.1, 7, 4);
