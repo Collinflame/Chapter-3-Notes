dee7671b3aa7d5bd0a17d15fedc42c81403fba49
3siy <3siy@fresnou.org>
2023-03-14 13:55:56 -0700

    initial commit

---
+++ FunctionNotes.js
@@ -0,0 +1,23 @@
+//Function Notes
+//Step 1 - Define the function
+// function sayHello (){
+//     console.log ('Hello');
+// }
+//Step 2 - Cell (use) the function
+// sayHello ();
+
+// function printMax (x, y){
+//     if (x > y){
+//         console.log(`the larger number is ${x}`)
+//     } else {
+//         console.log(`the larger number is ${y}`)
+//     }
+// }
+//
+// printMax (8, 21);
+
+function sum(number1, number2) {
+    console.log(`${number1} + ${number2} = ${number1 + number2}`)
+}
+
+sum (31,-23);
