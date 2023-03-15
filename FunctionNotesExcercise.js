dffd6b933073e389b7bac67540e19c539aaa4e91
3siy <3siy@fresnou.org>
2023-03-15 13:46:42 -0700

    Function Update

--- FunctionNotesExcercise.js
+++ FunctionNotesExcercise.js
@@ -39,3 +39,61 @@
 //     }
 // }
 // calculateLargestRectangle(4, 4.1, 7, 4);
+
+//Question 6
+// function charityCollection(donate1, donate2, donate3){
+//     if (donate1 + donate2 + donate3 >= 1000) {
+//         console.log (`We earned $${donate1 + donate2 + donate3} in donations so now it will be doubled so we have gotten donations of $${donate1 + donate1 + donate2 + donate3 + donate2 + donate3}!`)
+//     } else {
+//         console.log (`We earned $${donate1 + donate2 + donate3} in donations!`)
+//     }
+// }
+// charityCollection(400,200,800);
+
+//Question 7
+// function stateOfMatter(temp){
+//     if (temp >= 32 && temp <= 212) {
+//         console.log (`It's ${temp} degrees fahrenheit and the water will be liquid`)
+//     } else if (temp <= 32) {
+//         console.log (`It's ${temp} degrees fahrenheit and the water will be solid`)
+//     }  else {
+//         console.log (`It's ${temp} degrees fahrenheit and the water will be gas`)
+//     }
+// }
+// stateOfMatter(31);
+
+//Question 8
+// function findMaximumValue(num1, num2, num3){
+//     if (num1 > num2 && num1 > num3) {
+//         console.log (`${num1} is the largest number.`)
+//     } else if (num2 > num1 && num2 > num3) {
+//         console.log (`${num2} is the largest number.`)
+//     }  else {
+//         console.log (`${num3} is the largest number.`)
+//     }
+// }
+// findMaximumValue(31, 38, 29);
+
+//Question 9
+// function findMinimumValue(num1, num2, num3){
+//     if (num1 < num2 && num1 < num3) {
+//         console.log (`${num1} is the smallest number.`)
+//     } else if (num2 < num1 && num2 < num3) {
+//         console.log (`${num2} is the smallest number.`)
+//     }  else {
+//         console.log (`${num3} is the smallest number.`)
+//     }
+// }
+// findMinimumValue(31, 38, 29);
+
+//Question 10
+// function updatedCharityCollection(donates1, donates2, donates3){
+//     if (donates1 + donates2 + donates3 >= 1000 && donates1 + donates2 + donates3 <= 2000) {
+//         console.log (`We earned $${donates1 + donates2 + donates3} in donations so now it will be doubled so we have gotten donations of $${donates1 + donates1 + donates2 + donates3 + donates2 + donates3}!`)
+//     } else if (donates1 + donates2 + donates3 >= 2000) {
+//         console.log (`We earned $${donates1 + donates2 + donates3} in donations so now it will be plsu $2000 so we have gotten donations of $${2000 + donates1 + donates2 + donates3}!`)
+//     } else {
+//         console.log (`We earned $${donates1 + donates2 + donates3} in donations plus a $100 bonus means re reached $${donates1 + donates2 + donates3 + 100}!`)
+//     }
+// }
+// updatedCharityCollection(1000,200,800);
