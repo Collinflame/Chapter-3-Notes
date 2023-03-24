55e4be8791e00f2e91705bb641586a52904936fb
3siy <3siy@fresnou.org>
2023-03-24 14:17:45 -0700

    Arrays Update

--- ArraysNotesExcercise.js
+++ ArraysNotesExcercise.js
@@ -1,11 +1,33 @@
 //Question 1
-// let firstq = [5, 4, 3, 2, 1];
-// console.log(firstq[0]);
+// function firstelement (firstq) {
+//     console.log(firstq[0]);
+// }
+// firstelement([5,4,3,2,1]);
 
 //Question 2
-// let secondq = [5, 4, 3, 2, 1];
-// console.log(secondq[4]);
+// function secondelement (secondq) {
+//     console.log(secondq[4]);
+// }
+// secondelement([5,4,3,2,1]);
 
 //Question 3
-let thirdq = [5, 4, 3, 2, 1];
-console.log(thirdq[0]);
+// function thirdelement (thirdq) {
+//     if (thirdq.length == 0) {
+//         console.log("Empty Array")
+//     } else {
+//         console.log(thirdq[0])
+//     }
+// }
+// thirdelement([]);
+
+//Question 4
+// let fourthq = [5, 4, 3, 2, 1];
+// for(let i = fourthq.length -1; i >= 0; i--){
+//     console.log(fourthq[i]);
+// }
+
+//Question 5
+let fifthq = [6, 5, 4, 3, 2, 1];
+for(let i = fifthq.length -1; i >= 0; i--){
+    console.log(fifthq[i]);
+}   
