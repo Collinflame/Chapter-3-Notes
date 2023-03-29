5f0739b4d67de88a1b19e7cd282a12e8b03193f3
3siy <3siy@fresnou.org>
2023-03-29 13:43:58 -0700

    Arrays Update

--- ArraysNotes2Excercise.js
+++ ArraysNotes2Excercise.js
@@ -64,25 +64,84 @@
 // }
 // question5 ([1, 2, 3, 4, 5]);
 
-//Question 6
-function question6 (array6) {
-    let newarray6 = [];
-    let first = array6[0];
-    let last = array6.length - 0;
-    if (first > last) {
-        for (let i = 0; i < array6.length; i++) {
-            let x = first;
-            newarray6.push(array6[1]);
-            newarray6[i] = x;
-        }
-        console.log(newarray6)
-    } else {
-        for (let i = 0; i < array6.length; i++) {
-            let x = last;
-            newarray6.push(array5[1]);
-            newarray6[i] = x;
-        }
-        console.log(newarray5)
+// //Question 6
+// function question6 (array6) {
+//     let newarray6 = [];
+//     let first = array6[0];
+//     let last = array6.length - 0;
+//     if (first > last) {
+//         for (let i = 0; i < array6.length; i++) {
+//             let x = first;
+//             newarray6.push(array6[1]);
+//             newarray6[i] = x;
+//         }
+//         console.log(newarray6)
+//     } else {
+//         for (let i = 0; i < array6.length; i++) {
+//             let x = last;
+//             newarray6.push(array6[1]);
+//             newarray6[i] = x;
+//         }
+//         console.log(array6)
+//         console.log(newarray6)
+//     }
+// }
+// question6 ([1, 2, 3, 4, 5]);
+
+// //Question 7
+// function question7 (array7) {
+//     let newarray7 = [];
+//     let first = array7[0];
+//     let last = array7.length - 0;
+//     if (first < last) {
+//         for (let i = 0; i < array7.length; i++) {
+//             let x = first;
+//             newarray7.push(array7[1]);
+//             newarray7[i] = x;
+//         }
+//         console.log(newarray7)
+//     } else {
+//         for (let i = 0; i < array7.length; i++) {
+//             let x = last;
+//             newarray7.push(array7[1]);
+//             newarray7[i] = x;
+//         }
+//         console.log(newarray7);
+//     }
+// }
+// question7 ([1, 2, 3, 4, 5]);
+
+// //Question 8
+// function question8 (array8) {
+//     let newarray8 = [];
+//     let first = array8[0];
+//     let last = array8.length - 0;
+//     console.log(array8);
+//     if (first < last) {
+//         for (let i = 0; i < array8.length; i++) {
+//             let x = first;
+//             newarray8.push(array8[1]);
+//             newarray8[i] = x;
+//         }
+//         console.log(newarray8)
+//     } else {
+//         for (let i = 0; i < array8.length; i++) {
+//             let x = last;
+//             newarray8.push(array6[1]);
+//             newarray8[i] = x;
+//         }
+//         console.log(newarray8);
+//     }
+// }
+// question8 ([1, 2, 3, 4, 5]);
+
+//Question 9
+function question9 (array9) {
+    let newarray9 = [];
+    let i = array9[0] * array9[1] * array9[2] * array9[3] * array9[4];
+    for (let x = 0; x < array9.length; x++ ) {
+        newarray9.push(i / array9[x])
     }
+    console.log(newarray9)
 }
-question6 ([1, 2, 3, 4, 5]);
+question9 ([1, 2, 3, 4, 5]);
