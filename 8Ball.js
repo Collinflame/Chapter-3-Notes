dcd2b040244d6bda7549f4bb841dbf1a5234c520
3siy <3siy@fresnou.org>
2023-04-13 13:46:23 -0700

    8-Ball Update

---
+++ 8Ball.js
@@ -0,0 +1,8 @@
+const readLine = require('readline-sync');
+
+let answers = ['yes', 'no', 'maybe', 'whatever you want the answer to be that is the answer', 'i am not sure', 'nope'];
+let randomnum = Math.floor(Math.random()*answers.length);
+console.log('Welcome to My Magical 8-Ball. Remember this one rule. Only yes or no questions.');
+readLine.question('What is your question?: ');
+console.log(answers[randomnum]);
+
